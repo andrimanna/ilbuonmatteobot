c9c838ea5cf3100bb8f148e51cf8e319add12d82
Andrea Mannarella <andrimanna@users.noreply.github.com>
2018-01-21 15:21:01 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -3,4 +3,4 @@ title Il Buon Matteo Bot
 echo Avvio il bot...
 node.exe index.js
 pause
-rem aggiungi in questa directory ffmpeg.exe
+rem aggiungi in questa directory ffmpeg.exe e eseguire npm install
